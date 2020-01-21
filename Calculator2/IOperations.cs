fd9e62b2e578667bbcdccbce634e106f028321c6
Kazi Tushita Tahsin <kazi26@student.sust.edu>
2020-01-21 13:34:53 +0600

    Added operations interface

---
+++ Calculator2/IOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator2
+{
+    interface IOperations
+    {
+        public double calculate(double a, double b);
+    }
+}
