8df320d17fc90d889704757674c2f45c0985d426
Kazi Tushita Tahsin <kazi26@student.sust.edu>
2020-01-21 14:53:03 +0600

    struct

---
+++ Calculator2/ComplexAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator2
+{
+    struct ComplexAdd : IOperations
+    {
+        double real;
+        double imaginary;
+
+        public double calculate(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
