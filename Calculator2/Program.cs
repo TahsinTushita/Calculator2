0cd2ebd9df59041e3443636719e41c44d106b721
Kazi Tushita Tahsin <kazi26@student.sust.edu>
2020-01-22 15:31:43 +0600

    update

--- Calculator2/Program.cs
+++ Calculator2/Program.cs
@@ -8,24 +8,27 @@ namespace Calculator2
         {
             CalculatorFactory<RealNumber> realNum = new CalculatorFactory<RealNumber>();
             IOperations<RealNumber> operation = realNum.getOperation("+");
-            Console.WriteLine(operation.calculate(new RealNumber(1),
+            Console.WriteLine("result = " + operation.calculate(new RealNumber(1),
                 new RealNumber(2)).num);
 
             CalculatorFactory<ComplexNumber> compNum = new CalculatorFactory<ComplexNumber>
                 ();
             IOperations<ComplexNumber> operation1 = compNum.getOperation("+");
-            ComplexNumber cnum = new ComplexNumber();
-            cnum = operation1.calculate(
+            ComplexNumber complexNumber = new ComplexNumber();
+            complexNumber = operation1.calculate(
                 new ComplexNumber(2, 3), new ComplexNumber(4, 5));
-            Console.WriteLine("result = {0}+{1}i",cnum.realNum,cnum.imgNum);
+            Console.WriteLine("result = {0}+{1}i",complexNumber.realNum,
+                complexNumber.imgNum);
 
-            CalculatorFactory<VectorNumber> vectorNum = new CalculatorFactory<VectorNumber>();
-            IOperations<VectorNumber> operation2 = vectorNum.getOperation("+");
-            VectorNumber number = new VectorNumber();
-            number = operation2.calculate(new VectorNumber(2, 3, 4),
-                new VectorNumber(2, 3, 4));
+            CalculatorFactory<VectorNumber> vectorNum = new 
+                CalculatorFactory<VectorNumber>();
+            IOperations<VectorNumber> operation2 = vectorNum.getOperation("-");
+            VectorNumber vectorNumber = new VectorNumber();
+            vectorNumber = operation2.calculate(new VectorNumber(2, 3, 4),
+                new VectorNumber(-2, 3,-4));
 
-            Console.WriteLine("result = {0}i+{1}j+{2}k", number.u,number.v,number.w);
+            Console.WriteLine("result = {0}i+{1}j+{2}k", vectorNumber.u,
+                vectorNumber.v,vectorNumber.w);
         }
     }
 }
